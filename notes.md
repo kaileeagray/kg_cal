ac43d01aa88427bfbf2c225dc8dad85569dbdbba
Kailee <kaileeagray@gmail.com>
2016-10-24 18:33:41 -0500

    comment js

--- notes.md
+++ notes.md
@@ -1,5 +1,8 @@
 # ToDos
 !!!+ ability to delete events
++ visually separate js code
++ sat/sun diff color?
++ create repeating events
 + enhance mobile view
 + in week view, add ability to click on text of day to go to day as well as in day
 + in week view, if user clicks on a time slot, open that slot in day view, then
--- public/js/index.js
+++ public/js/index.js
@@ -1,40 +1,47 @@
 $(document).ready(function() {
-  $('.calendar').fullCalendar({
-      editable: true,
-      eventLimit: true, // allow "more" link when too many events
-      events: [{
-          "title": "All Day Event",
-          "start": "2016-10-01"
-      }, {
-          "title": "Long Event",
-          "start": "2016-10-07",
-          "end": "2016-10-10"
-      }, {
-          "id": "999",
-          "title": "Repeating Event",
-          "start": "2016-10-10T16:00:00-05:00"
-      }, {
-          "id": "999",
-          "title": "Repeating Event",
-          "start": "2016-10-16T16:00:00-05:00"
-      }, {
-          "title": "Conference",
-          "start": "2016-10-11",
-          "end": "2016-10-13"
-      }, {
-          "title": "Meeting",
-          "start": "2016-10-12T10:30:00-05:00",
-          "end": "2016-10-12T12:30:00-05:00"
-      }, {
-          "title": "Lunch",
-          "start": "2016-10-12T12:00:00-05:00"
-      }]
-  });
 
+  /* -------------------------------cache DOM------------------------------- */
+  //$cal1 and $cal2 are part of $cal
   var $cal = $('.calendar');
   var $cal1 = $('#calendar1');
   var $cal2 = $('#calendar2');
 
+
+  /* --------------------------initialize calendar-------------------------- */
+  $('.calendar').fullCalendar({
+      editable: true,
+      eventLimit: true, // allow "more" link when too many events
+      // create events
+        events: [{
+            "title": "All Day Event",
+            "start": "2016-10-01"
+        }, {
+            "title": "Long Event",
+            "start": "2016-10-07",
+            "end": "2016-10-10"
+        }, {
+            "id": "999",
+            "title": "Repeating Event",
+            "start": "2016-10-10T16:00:00-05:00"
+        }, {
+            "id": "999",
+            "title": "Repeating Event",
+            "start": "2016-10-16T16:00:00-05:00"
+        }, {
+            "title": "Conference",
+            "start": "2016-10-11",
+            "end": "2016-10-13"
+        }, {
+            "title": "Meeting",
+            "start": "2016-10-12T10:30:00-05:00",
+            "end": "2016-10-12T12:30:00-05:00"
+        }, {
+            "title": "Run tests",
+            "start": "2016-10-12T12:00:00-05:00"
+        }]
+    });
+
+  /* -------------------create, manage cal2 (right pane)------------------- */
   $cal2.fullCalendar('changeView', 'agendaDay');
 
   $cal2.fullCalendar('option', {
@@ -78,6 +85,7 @@ $(document).ready(function() {
   });
 
 
+  /* -------------------create, manage cal1 (right pane)------------------- */
   $cal1.fullCalendar('option', {
       header: {
           left: 'prev,next today',
@@ -93,10 +101,18 @@ $(document).ready(function() {
       }
   });
 
+
+
+  /* -------------------moves right pane to date------------------- */
   function cal2GoTo(date) {
       $cal2.fullCalendar('gotoDate', date);
   }
 
+
+  /* full calendar gives all day events given different ids in month/week view
+    and day view. we create/edit event in day (right) view, so correct for
+    id change to update in month/week (left)
+  */
   function getCal1Event(cal2Id) {
       var num = cal2Id.slice(-1) - 5;
       var id = "_fc" + num;
